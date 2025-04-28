6aca831a0c4814e59990db154d04fb3ec91e1ba1
saksham <phantomdx316@gmail.com>
2025-04-28 15:15:40 +0530

    change auth

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 
 const useAuth = () => {
-  const email = useSelector((state) => state.auth.user.email ?? "");
+  const email = useSelector((state) => state?.auth?.user?.email ?? "");
 
   return { email };
 };
